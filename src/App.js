442b44610bb7af0759a25f8ec9a37508416b6af7
Kir <kir.dontsov@gmail.com>
2020-06-10 16:29:32 +0300

    10.06.2020

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Helmet } from "react-helmet";
+import { YMInitializer } from "react-yandex-metrika";
 
 import { routes } from "./routes";
 
@@ -13,83 +14,73 @@ import "./scss/App.scss";
 const supportsHistory = "pushState" in window.history;
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      width: window.innerWidth
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			width: window.innerWidth
+		};
+	}
 
-  componentDidMount() {
-    window.addEventListener("resize", this.handleWindowSizeChange);
-  }
+	componentDidMount() {
+		window.addEventListener("resize", this.handleWindowSizeChange);
+	}
 
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.handleWindowSizeChange);
-  }
+	componentWillUnmount() {
+		window.removeEventListener("resize", this.handleWindowSizeChange);
+	}
 
-  handleWindowSizeChange = () => {
-    this.setState({ width: window.innerWidth });
-  };
+	handleWindowSizeChange = () => {
+		this.setState({ width: window.innerWidth });
+	};
 
-  render() {
-    const { width } = this.state;
-    const isMobile = width <= 768;
+	render() {
+		const { width } = this.state;
+		const isMobile = width <= 768;
 
-    const renderSwitch = () => (
-      <Switch>
-        {routes.map(route => {
-          const component = route.component;
-          return (
-            <Route
-              key={route.id}
-              exact={route.isExact}
-              path={route.path}
-              component={component}
-              status={route.status}
-            />
-          );
-        })}
-      </Switch>
-    );
+		const renderSwitch = () => (
+			<Switch>
+				{routes.map(route => {
+					const component = route.component;
+					return <Route key={route.id} exact={route.isExact} path={route.path} component={component} status={route.status} />;
+				})}
+			</Switch>
+		);
 
-    if (isMobile) {
-      return (
-        <Router forceRefresh={!supportsHistory}>
-          <Fragment>
-            <Helmet>
-              <title>РТИ-Торг</title>
-              <meta name="description" content="РТИ-Торг" />
-            </Helmet>
-            <div className="wrapper">
-              <Burger routes={routes.filter(route => route.isMobile)} />
-              {/* <h1>Mobile</h1> */}
-              {renderSwitch()}
-              <Footer routes={routes.filter(route => route.isFooter)} />
-            </div>
-          </Fragment>
-        </Router>
-      );
-    } else {
-      return (
-        <Router forceRefresh={!supportsHistory}>
-          <Fragment>
-            <Helmet>
-              <title>РТИ-Торг</title>
-              <meta name="description" content="РТИ-Торг" />
-            </Helmet>
+		if (isMobile) {
+			return (
+				<Router forceRefresh={!supportsHistory}>
+					<YMInitializer accounts={[64769932]} options={{ webvisor: true }} />
+					<Helmet>
+						<title>РТИ-Торг</title>
+						<meta name="description" content="РТИ-Торг" />
+					</Helmet>
+					<div className="wrapper">
+						<Burger routes={routes.filter(route => route.isMobile)} />
+						{/* <h1>Mobile</h1> */}
+						{renderSwitch()}
+						<Footer routes={routes.filter(route => route.isFooter)} />
+					</div>
+				</Router>
+			);
+		} else {
+			return (
+				<Router forceRefresh={!supportsHistory}>
+					<YMInitializer accounts={[64769932]} options={{ webvisor: true }} />
+					<Helmet>
+						<title>РТИ-Торг</title>
+						<meta name="description" content="РТИ-Торг" />
+					</Helmet>
 
-            <div className="wrapper">
-              <Shutter />
-              <NavigationBar routes={routes.filter(route => route.isNavBar)} />
-              {renderSwitch()}
-              <Footer routes={routes.filter(route => route.isFooter)} />
-            </div>
-          </Fragment>
-        </Router>
-      );
-    }
-  }
+					<div className="wrapper">
+						<Shutter />
+						<NavigationBar routes={routes.filter(route => route.isNavBar)} />
+						{renderSwitch()}
+						<Footer routes={routes.filter(route => route.isFooter)} />
+					</div>
+				</Router>
+			);
+		}
+	}
 }
 
 export default App;
